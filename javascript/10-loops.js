0116bf20078343960c67aad163ad0bf6f85bf4c2
dennis enang oyama <dennisenang2018@gmail.com>
2025-03-27 16:48:24 +0100

    changes made with the navbar

---
+++ javascript/10-loops.js
@@ -0,0 +1,36 @@
+// alert("Hello world 1");
+// alert("Hello world 2");
+// alert("Hello world 3");
+// alert("Hello world 4");
+// alert("Hello world 5");
+// alert("Hello world 6");
+// alert("hello world 7");
+// alert("hello world 8");
+// alert("Hello world 9");
+// alert("Hello world 10");
+
+// loops helps us inwriting repetitive code in an easier and shorter way
+// use for loop
+
+for (let i = 1; i <= 10; i++) {
+  console.log("2 * " + i + "=" + i * 2);
+}
+
+const usernames = ["yusuf", "Ahmed", "jubril", "musa", "paul", "micheal"];
+
+console.log("Number of user is ", usernames.length);
+
+// console.log(usernames[0])
+// console,log(username[1])
+// console.log(usernames[2])
+// console.log(usernames[3])
+
+for (let i = 0; i <= 3; i++) {
+  console.log(usernames[i]);
+}
+
+const cars = ["Tesla", "Ford", "Benz"];
+
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
